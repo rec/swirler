7da347e0850d20aa7709ec38893b2205a80afb62
Tom Ritchford <tom@swirly.com>
2016-01-18 17:47:46 -0500

    Rename to swirler.

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='swirly runner',
-    url='http://github.com/rec/runner',
+    name='swirler',
+    url='http://github.com/rec/swirler',
     version='0.1',
-    packages=['runner',],
-    dependency_links=[
-        'http://github.com/ManiacalLabs/BiblioPixel/tarball/master#egg=bibliopixel',
-        'http://github.com/ManiacalLabs/PixelWeb/tarball/master#egg=pixelweb',
-    ],
-    install_requires=['bibliopixel', 'pixelweb',],
+    packages=['swirler',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
